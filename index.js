136e2d7c5f6172a3d0cbb2092518a17ad75f084e
Joemel <bituinjoemel@gmail.com>
2021-02-16 21:07:40 +0800

    Add message response handlers

--- index.js
+++ index.js
@@ -6,15 +6,63 @@ const express = require('express'),
   axios = require('axios'),
   app = express().use(bodyParser.json()); // creates express http server
 
-app.get('/', function (req, res) {
+const COMMANDS = {
+  DOGE: 'DOGE',
+};
+
+function getDogeValue() {
+  return axios.get(process.env.DOGE_URL);
+}
+// Handles messages events
+function handleMessage(senderPSID, receivedMessage) {
+  let response;
+
+  // Check if the message contains text
+  if (receivedMessage.text) {
+    // Create the payload for a basic text message
+    response = {
+      text: `You sent the message: "${receivedMessage.text}".`,
+    };
+
+    // Sends the sample response message
+    callSendAPI(senderPSID, response);
+  }
+
+  if (receivedMessage.text == COMMANDS.DOGE) {
+    getDogeValue()
+      .then(({ data }) => {
+        response.text = `Value @ ${data}`;
+        callSendAPI(senderPSID, response);
+      })
+      .error(() => console.err(err));
+  }
+}
+
+// Sends response messages via the Send API
+function callSendAPI(senderPSID, response) {
+  let requestBody = {
+    recipient: {
+      id: senderPSID,
+    },
+    message: response,
+  };
+
   axios
-    .get('https://www.binance.com/api/v3/ticker/price?symbol=DOGEBUSD')
-    .then(({ data }) => {
-      const { price } = data;
-      res.json(price);
+    .post('https://graph.facebook.com/v2.6/me/messages', {
+      headers: {
+        access_token: process.env.PAGE_ACCESS_TOKEN,
+      },
+      data: {
+        ...requestBody,
+      },
     })
-    .catch((err) => console.log('err', err));
-});
+    .then(() => {
+      console.log('message sent');
+    })
+    .error((err) => {
+      console.error('Unable to send message:' + err);
+    });
+}
 
 app.get('/webhook', function (req, res) {
   // Your verify token. Should be a random string.
@@ -42,15 +90,24 @@ app.get('/webhook', function (req, res) {
 // Creates the endpoint for our webhook
 app.post('/webhook', (req, res) => {
   let body = req.body;
-
   // Checks this is an event from a page subscription
   if (body.object === 'page') {
     // Iterates over each entry - there may be multiple if batched
     body.entry.forEach(function (entry) {
       // Gets the message. entry.messaging is an array, but
       // will only ever contain one message, so we get index 0
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
+      let webhookEvent = entry.messaging[0];
+      console.log(webhookEvent);
+
+      // Get the sender PSID
+      let senderPSID = webhookEvent.sender.id;
+      console.log('Sender PSID: ' + senderPSID);
+
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhookEvent.message) {
+        handleMessage(senderPSID, webhookEvent.message);
+      }
     });
 
     // Returns a '200 OK' response to all requests
@@ -61,6 +118,7 @@ app.post('/webhook', (req, res) => {
   }
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Example app listening on port 3000!');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
+  console.log(`App listening on port: ${PORT}`);
 });
